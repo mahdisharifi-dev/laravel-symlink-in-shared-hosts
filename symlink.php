fd8347d13ad345af275016c315ef1f5c065a1c17
Mahdi Sharifi <mahdi.sh.twd@gmail.com>
2020-09-03 23:04:20 +0430

    Update symlink.php

--- symlink.php
+++ symlink.php
@@ -7,3 +7,6 @@ $shortcut = 'storage';
 symlink($target, $shortcut);
 
 // put this file in your public_html directory then run www.youtSite.com/symlink.php
+
+// if you get error try it with Cron Job
+// ln -s /home/afkmuxpo/YOUR_APP_DIRECTORY/storage/app/public /home/afkmuxpo/YOUR_PUBLIC_DRIECTORY/storage
