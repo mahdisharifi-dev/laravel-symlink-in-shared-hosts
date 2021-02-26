ebef1247903ba115ea2b359afae9e5efb1b3f145
Mahdi Sharifi <mahdi.sh.twd@gmail.com>
2021-02-26 15:50:20 +0330

    Update symlink.php

--- symlink.php
+++ symlink.php
@@ -9,4 +9,4 @@ symlink($target, $shortcut);
 // put this file in your public_html directory then run www.youtSite.com/symlink.php
 
 // if you get error try it with Cron Job
-// ln -s /home/afkmuxpo/YOUR_APP_DIRECTORY/storage/app/public /home/afkmuxpo/YOUR_PUBLIC_DRIECTORY/storage
+// ln -s /home/YOUR_HOST_NAME/YOUR_APP_DIRECTORY/storage/app/public /home/YOUR_HOST_NAME/YOUR_PUBLIC_DRIECTORY/storage
